246102c47299f8b9d38219534be42cec92a13479
Hugo <hugo_mendoza_s@outlook.com>
2024-04-13 10:20:31 -0500

    feat: create logic for send email from node

--- 05-noc/src/domain/entities/log.entity.ts
+++ 05-noc/src/domain/entities/log.entity.ts
@@ -4,23 +4,37 @@ export enum LogSeverityLevel {
   high = 'high'
 }
 
+export interface logEntityOptions {
+  level: LogSeverityLevel;
+  message: string;
+  origin: string;
+  createdAt?: Date;
+}
+
 export class LogEntity {
 
   public level: LogSeverityLevel;
-  public messsage: string;
+  public message: string;
   public createdAt: Date;
+  public origin: string;
 
-  constructor( message: string, level: LogSeverityLevel ) {
+  constructor( options: logEntityOptions ) {
+    const { level, message, origin, createdAt = new Date() } = options
     this.level = level;
-    this.messsage = message;
-    this.createdAt = new Date();
+    this.message = message;
+    this.createdAt = createdAt;
+    this.origin = origin;
   }
 
   static fromJson = ( json: string ):LogEntity => {
-    const { message, level, createdAt } = JSON.parse( json )
+    const { message, level, createdAt, origin } = JSON.parse( json )
 
-    const log = new LogEntity( message, level );
-    log.createdAt = new Date( createdAt );
+    const log = new LogEntity({
+      message,
+      level,
+      createdAt,
+      origin
+    });
 
     return log
   }
--- 05-noc/src/presentation/server.ts
+++ 05-noc/src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repositry.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource()
@@ -28,17 +29,30 @@ const fileSystemLogRepository = new LogRepositoryImpl(
 export class Server {
   public static start() {
     console.log('Server started...');
-    CronService.createJob(
-      '*/5 * * * * *',
-      () =>  {
-        const url = 'https://www.localhost:3000'
-        new CheckService(
-          fileSystemLogRepository,
-          undefined,
-          undefined
-        ).execute(url)
-        // new CheckService().execute('http://localhost:3000/')
-      }
-    );
+
+    // * mandar email
+    const emailService = new EmailService();
+    emailService.sendEmail({
+      to: 'hams21@gmail.com',
+      subject: 'Test',
+      htmlBody: `
+        <h1>Test</h1>
+        <p>Test</p>
+        <p>Test</p>
+      `
+    })
+
+    // CronService.createJob(
+    //   '*/5 * * * * *',
+    //   () =>  {
+    //     const url = 'https://www.localhost:3000'
+    //     new CheckService(
+    //       fileSystemLogRepository,
+    //       undefined,
+    //       undefined
+    //     ).execute(url)
+    //     // new CheckService().execute('http://localhost:3000/')
+    //   }
+    // );
   }
 }
